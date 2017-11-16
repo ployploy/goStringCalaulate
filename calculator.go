bdd90f61f9d38bc389a7bf48335d04c740a905b6
ploy <chonnikan.t@kkumail.com>
2017-11-16 16:38:03 +0700

    Insert test input 1 and 2 output 3

--- calculator.go
+++ calculator.go
@@ -1,8 +1,26 @@
 package Test
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Calculate struct {
 }
 
-func (Calculate) add(number string) string {
-	return "0"
+func (Calculate) sum(number string) int {
+	if strings.Contains(number, ",") {
+		numbers := strings.Split(number, ",")
+		sum := 0
+		for _, value := range numbers {
+			n, _ := strconv.Atoi(value)
+			sum = n + sum
+		}
+		return sum
+	}
+
+	if number == "1" {
+		return 1
+	}
+	return 0
 }
--- calculator_test.go
+++ calculator_test.go
@@ -4,8 +4,25 @@ import "testing"
 
 func TestInputemplySholudOutputzero(t *testing.T) {
 	cal := Calculate{}
-	result := cal.add("")
-	expected := "0"
+	result := cal.sum("")
+	expected := 0
+	if result != expected {
+		t.Fatal("Expected ", expected, "but actual", result)
+	}
+}
+func TestInput1SholudOutput1(t *testing.T) {
+	cal := Calculate{}
+	result := cal.sum("1")
+	expected := 1
+	if result != expected {
+		t.Fatal("Expected ", expected, "but actual", result)
+	}
+}
+
+func TestInput1and2ShouldOutput3(t *testing.T) {
+	cal := Calculate{}
+	result := cal.sum("1,2")
+	expected := 3
 	if result != expected {
 		t.Fatal("Expected ", expected, "but actual", result)
 	}
